54a1d773aae614aeaa894ed90b139f8172804e4a
Ashish <aashish7322@gmail.com>
2022-09-15 12:24:17 +0530

    Calculated total montly employee Wage

--- src/main/java/com/bridgelabz/javasamples/App.java
+++ src/main/java/com/bridgelabz/javasamples/App.java
@@ -14,31 +14,36 @@ public class App {
 		// int isPartTime = 2;
 		int WagePerHour = 20;
 		int DailyEmpWage = 0;
+		int TotalMonthlyWorkingHours = 0;
+		int TotalEmpWage=0;
 
 		System.out.println("Welcome to Employee Wage Computation Program");
+		for (int i = 1; i <= 20; i++) {
+			Random random = new Random();
+			int randomcheck = random.nextInt(3);
 
-		Random random = new Random();
-		int randomcheck = random.nextInt(3);
-		
-		switch (randomcheck) {
-		case (1): {
-			System.out.println("Employee is Present");
-			WorkingHours = 8;
-			break;
+			switch (randomcheck) {
+			case (1): {
+				System.out.println("Employee is Present");
+				WorkingHours = 8;
+				break;
+			}
+			case (2): {
+				System.out.println("Employee is Present,but worked for PartTime");
+				WorkingHours = 4;
+				break;
+			}
+			case (0): {
+				System.out.println("Employee is Absent");
+				DailyEmpWage = 0;
+				break;
+			}
+
+			}
+			TotalMonthlyWorkingHours += WorkingHours;
 		}
-		case (2): {
-			System.out.println("Employee is Present,but worked for PartTime");
-			WorkingHours = 4;
-			break;
-		}
-		case (0): {
-			System.out.println("Employee is Absent");
-			DailyEmpWage = 0;
-			break;
-		}
-		}
-		DailyEmpWage = WorkingHours * WagePerHour;
-		System.out.println("Daily Employee Wage of Employee is :" + DailyEmpWage);
+		TotalEmpWage = TotalMonthlyWorkingHours * WagePerHour;
+		System.out.println("Total Monthly Employee Wage of Employee is :" + TotalEmpWage);
 
 	}
 }
