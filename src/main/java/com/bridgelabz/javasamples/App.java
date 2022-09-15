0db42ed9d0e22c8abecfc3b6e31788b50584ab9e
Ashish <aashish7322@gmail.com>
2022-09-15 11:31:38 +0530

    DailyEmployeeWage

--- src/main/java/com/bridgelabz/javasamples/App.java
+++ src/main/java/com/bridgelabz/javasamples/App.java
@@ -9,16 +9,25 @@ import java.util.Random;
 public class App {
 
 	public static void main(String[] args) {
+		int FullDayHour = 0;
+		int isPresent = 1;
+		int WagePerHour = 20;
+		int DailyEmpWage = 0;
 
 		System.out.println("Welcome to Employee Wage Computation Program");
 
-		int isPresent = 1;
 		Random random = new Random();
 		int randomcheck = random.nextInt(2);
 
-		if (isPresent == randomcheck)
+		if (isPresent == randomcheck) {
 			System.out.println("Employee is Present");
-		else
+			FullDayHour = 8;
+			DailyEmpWage = FullDayHour * WagePerHour;
+
+		} else {
 			System.out.println("Employee is Absent");
+			DailyEmpWage = 0;
+		}
+		System.out.println("Daily Employee Wage of Employee is :" + DailyEmpWage);
 	}
 }
