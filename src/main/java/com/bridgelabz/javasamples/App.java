a2524e10b5eb28bbc4e08d90f4dcae5cf67a6113
Ashish <aashish7322@gmail.com>
2022-09-15 11:16:35 +0530

    Check Employee is Present or Absent

--- src/main/java/com/bridgelabz/javasamples/App.java
+++ src/main/java/com/bridgelabz/javasamples/App.java
@@ -1,5 +1,7 @@
 package com.bridgelabz.javasamples;
 
+import java.util.Random;
+
 /**
  * Hello world!
  *
@@ -9,5 +11,14 @@ public class App {
 	public static void main(String[] args) {
 
 		System.out.println("Welcome to Employee Wage Computation Program");
+
+		int isPresent = 1;
+		Random random = new Random();
+		int randomcheck = random.nextInt(2);
+
+		if (isPresent == randomcheck)
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is Absent");
 	}
 }
