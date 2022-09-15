334a73ade4a4c4cd8e64179d76b207a284ec374a
Ashish <aashish7322@gmail.com>
2022-09-15 12:43:20 +0530

    MonthlyWageConditionMet

--- src/main/java/com/bridgelabz/javasamples/App.java
+++ src/main/java/com/bridgelabz/javasamples/App.java
@@ -15,10 +15,12 @@ public class App {
 		int WagePerHour = 20;
 		int DailyEmpWage = 0;
 		int TotalMonthlyWorkingHours = 0;
+		int TotalWorkingDays=0;
 		int TotalEmpWage=0;
+		int count=0;
 
 		System.out.println("Welcome to Employee Wage Computation Program");
-		for (int i = 1; i <= 20; i++) {
+		for (int i = 1; i <= 30; i++) {
 			Random random = new Random();
 			int randomcheck = random.nextInt(3);
 
@@ -26,11 +28,13 @@ public class App {
 			case (1): {
 				System.out.println("Employee is Present");
 				WorkingHours = 8;
+				count++;
 				break;
 			}
 			case (2): {
 				System.out.println("Employee is Present,but worked for PartTime");
 				WorkingHours = 4;
+				count++;
 				break;
 			}
 			case (0): {
@@ -41,9 +45,14 @@ public class App {
 
 			}
 			TotalMonthlyWorkingHours += WorkingHours;
+			TotalWorkingDays +=count;
 		}
+		
+		if(TotalMonthlyWorkingHours>=100 && TotalWorkingDays>=20 ) {
 		TotalEmpWage = TotalMonthlyWorkingHours * WagePerHour;
 		System.out.println("Total Monthly Employee Wage of Employee is :" + TotalEmpWage);
-
+		}
+		else
+			System.out.println("Total monthly Working hours not met");
 	}
 }
