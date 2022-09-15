5c0f3e1ca820ff0d0c549907c297d558bdf5aefa
Ashish <aashish7322@gmail.com>
2022-09-15 11:48:05 +0530

    Added EmployeePart Time Wage

--- src/main/java/com/bridgelabz/javasamples/App.java
+++ src/main/java/com/bridgelabz/javasamples/App.java
@@ -9,25 +9,30 @@ import java.util.Random;
 public class App {
 
 	public static void main(String[] args) {
-		int FullDayHour = 0;
+		int WorkingHours = 0;
 		int isPresent = 1;
+		int isPartTime = 2;
 		int WagePerHour = 20;
 		int DailyEmpWage = 0;
 
 		System.out.println("Welcome to Employee Wage Computation Program");
 
 		Random random = new Random();
-		int randomcheck = random.nextInt(2);
+		int randomcheck = random.nextInt(3);
 
 		if (isPresent == randomcheck) {
 			System.out.println("Employee is Present");
-			FullDayHour = 8;
-			DailyEmpWage = FullDayHour * WagePerHour;
+			WorkingHours = 8;
+
+		} else if (isPartTime == randomcheck) {
+			System.out.println("Employee is Present,but worked for PartTime");
+			WorkingHours = 4;
 
 		} else {
 			System.out.println("Employee is Absent");
 			DailyEmpWage = 0;
 		}
+		DailyEmpWage = WorkingHours * WagePerHour;
 		System.out.println("Daily Employee Wage of Employee is :" + DailyEmpWage);
 	}
 }
