d5ca24942c73972fd23ff2436ef95371821b8071
Ashish <aashish7322@gmail.com>
2022-09-17 10:58:26 +0530

    Address Book with Contacts

--- src/main/java/com/bridgelabz/addressBook/AddressBook.java
+++ src/main/java/com/bridgelabz/addressBook/AddressBook.java
@@ -1,8 +1,39 @@
 package com.bridgelabz.addressBook;
 
+class Contacts{
+	
+	
+	String FirstName;
+	String LastName;
+	String Address;
+	String City;
+	String State;
+	String PhoneNO;
+	int ZipCode;
+	String Email;
+	
+	public Contacts(String firstName, String address, String city, String state, String phoneNO, int zipCode,
+			String email) {
+		super();
+		FirstName = firstName;
+		Address = address;
+		City = city;
+		State = state;
+		PhoneNO = phoneNO;
+		ZipCode = zipCode;
+		Email = email;
+	}
+	
+	void showContacts() {
+		System.out.println("FirstName : "+FirstName +"\nAddress: "+Address +" \nEmail : " +Email);
+	}
+}
 public class AddressBook {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Address Book");
+		
+		Contacts contact= new Contacts("Ashish","Burlington","Lucknow","UP","9044855917",226001,"aashish7322@gmail.com");
+		contact.showContacts();
 	}
 }
